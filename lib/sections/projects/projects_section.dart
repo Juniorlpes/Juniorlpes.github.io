5af69de701b14115eb1ca81f8a04d13a995290e5
Nilton Bento Júnior <nilton.bento@iatec.com>
2022-12-02 15:08:10 -0300

    -

--- lib/sections/projects/projects_section.dart
+++ lib/sections/projects/projects_section.dart
@@ -1,6 +1,13 @@
 import 'package:flutter/material.dart';
 
 //TODO: Projects
+/*
+  - 7me
+  - Hunno
+  - flyTrack
+  - Agenda ANPa
+  - ProExtrato
+*/
 
 class ProjectsSection extends StatelessWidget {
   const ProjectsSection({Key? key}) : super(key: key);
